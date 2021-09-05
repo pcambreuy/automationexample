29aab60811f05aa83ba873eb9194ad5fff0e50fa
Pablo Cambre <pablo.cambrelagorio@jumbo.com>
2021-09-05 16:05:02 +0200

    Config template to be completed

--- src/test/java/sample/CryptoTest.java
+++ src/test/java/sample/CryptoTest.java
@@ -20,10 +20,10 @@ public class CryptoTest {
         // Capabilities full list https://appium.io/docs/en/writing-running-appium/caps/
 
         DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setCapability("deviceName", "Pixel 4 API 30");
-        caps.setCapability("udid", "emulator-5556"); //DeviceId from "adb devices" command
-        caps.setCapability("platformName", "Android");
-        caps.setCapability("platformVersion", "11.0");
+        caps.setCapability("deviceName", "XXXXX");
+        caps.setCapability("udid", "XXXX"); //DeviceId from "adb devices" command
+        caps.setCapability("platformName", "XXXXX");
+        caps.setCapability("platformVersion", "XXXX");
         // Reset strategies https://appium.io/docs/en/writing-running-appium/other/reset-strategies/index.html
         caps.setCapability("noReset", "false");
 
@@ -33,8 +33,8 @@ public class CryptoTest {
 
         // https://stackoverflow.com/questions/13193592/adb-android-getting-the-name-of-the-current-activity
         // adb shell dumpsys window windows | grep -E 'mObscuringWindow' For Android 29
-        caps.setCapability("appPackage", "com.example.automationexample");
-        caps.setCapability("appActivity", "com.example.automationexample.MainActivity");
+        caps.setCapability("appPackage", "XXXXX");
+        caps.setCapability("appActivity", "XXXXX");
 
         driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), caps);
         wait = new WebDriverWait(driver, 10);
